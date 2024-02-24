8cada9370cb5593d24832c8e02d436e6ff600019
Risav03 <needleweed03@gmail.com>
2024-02-24 10:10:32 +0530

    new raffle contract

--- utils/contractAdds.jsx
+++ utils/contractAdds.jsx
@@ -2,7 +2,7 @@
 
 export const contractAdds = {
     stakingPearls : "0xA113c5FD816d7F51560d2B51dF70a4e05132eC75",
-    pearlRaffle: "0x772DE548A2dB80DA6be4D0dA463e18aC35FFc822",
+    pearlRaffle: "0xAa62E3245947e1a6954BFcd72b823Cd88697AF88",
     pearlToken: "0xC538FfB85D21Cc29746F1Dc15c4b9A1b282458da",
     pearlNFT: "0x76840d45545c525be93576864288A7e2800144c7",
     pearlMinimart: "0xC538FfB85D21Cc29746F1Dc15c4b9A1b282458da"
--- utils/newAbis/raffleabi.js
+++ utils/newAbis/raffleabi.js
@@ -420,24 +420,5 @@ export default [
 		],
 		"stateMutability": "view",
 		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "winningAddress",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
 	}
-]
+];
