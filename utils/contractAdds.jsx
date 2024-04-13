ef191470c1545b64714dac8276c40982e36874da
Risav03 <needleweed03@gmail.com>
2024-04-13 15:46:18 +0530

    contract updated

--- utils/contractAdds.jsx
+++ utils/contractAdds.jsx
@@ -2,7 +2,7 @@
 
 export const contractAdds = {
     stakingPearls : "0xA113c5FD816d7F51560d2B51dF70a4e05132eC75",
-    pearlRaffle: "0xf6C4CE0C7B2251d5546A0F7dA2Ff5e318d380016",
+    pearlRaffle: "0xFCaf7775060bA547bd902183F6b836b9E87bbA03",
     pearlToken: "0x58243b7c159d91Ef674CAEA7B98a483AA323aA8F",
     pearlNFT: "0x76840d45545c525be93576864288A7e2800144c7",
     pearlMinimart: "0xC538FfB85D21Cc29746F1Dc15c4b9A1b282458da",
